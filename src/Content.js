33004affe452c631faa4918542b3f2bd15469038
harwarl <harwarl87@gmail.com>
2023-02-19 15:34:23 -0800

    using set State

--- src/Content.js
+++ src/Content.js
@@ -1,31 +1,31 @@
 import React from 'react'
+import { useState } from 'react';
 
 const Content = () => {
+
+    const [name, setName] = useState('Awwal');
     const handleChangeName = () => {
         const names = ['Bob', 'Mathew', 'Kelvin'];
         const int = Math.floor(Math.random() * 3);
-        return names[int];
+        setName(names[int]);
     }
-    const handleClick = () =>{
-        console.log('You Clicked It')
-    }
+    // const handleClick = () =>{
+    //     console.log('You Clicked It')
+    // }
     const handleClick2 = (name) =>{
         console.log(`${name} was clicked`)
     }
     const handleClick3 = (e) =>{
         console.log(e.target.innerText)
     }
-    const handleClick4 = (name) =>{
-        console.log(`${name} Clicked It`)
-    }
 
     return (
         <main>
-            <p onDoubleClick={() => handleClick4(handleChangeName())}>
-                Hello {handleChangeName()}
+            <p>
+                Hello {name}!
             </p>
-            <button onClick={handleClick}>
-                Click It
+            <button onClick={handleChangeName}>
+                Change Name
             </button>
             <button onClick={()=>handleClick2('Dave')}>
                 Click It
