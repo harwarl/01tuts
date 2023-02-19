e3841de2d50364c163deed90ed50c79f3d1d105c
harwarl <harwarl87@gmail.com>
2023-02-19 15:43:27 -0800

    count in a function does not change

--- src/Content.js
+++ src/Content.js
@@ -4,19 +4,25 @@ import { useState } from 'react';
 const Content = () => {
 
     const [name, setName] = useState('Awwal');
+    const [count, setCount] = useState(0);
     const handleChangeName = () => {
         const names = ['Bob', 'Mathew', 'Kelvin'];
         const int = Math.floor(Math.random() * 3);
         setName(names[int]);
     }
-    // const handleClick = () =>{
-    //     console.log('You Clicked It')
-    // }
+    const handleClick1 = () =>{
+        setCount(count + 1);
+        console.log(count);
+    }
+
     const handleClick2 = (name) =>{
         console.log(`${name} was clicked`)
     }
     const handleClick3 = (e) =>{
         console.log(e.target.innerText)
+    }
+    const handleClick4 = () =>{
+        console.log(count);
     }
 
     return (
@@ -26,6 +32,12 @@ const Content = () => {
             </p>
             <button onClick={handleChangeName}>
                 Change Name
+            </button>
+            <button onClick={handleClick1}>
+                Increase Count
+            </button>
+            <button onClick={handleClick4}>
+                Check Count
             </button>
             <button onClick={()=>handleClick2('Dave')}>
                 Click It
